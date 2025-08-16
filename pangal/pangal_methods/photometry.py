7104bcef3fd1953bc255f57a7e43262ce8aebfb9
robertoinfurna <robertoinfurna1@gmail.com>
2025-08-16 23:19:40 +0200

    nice inspect photometry

--- pangal/pangal_methods/photometry.py
+++ pangal/pangal_methods/photometry.py
@@ -375,15 +375,21 @@ def surface_brightness_profile(self,regions,units='mJy_arcsec2',bands=None,thres
 
 
 
+from matplotlib.ticker import MaxNLocator
+from matplotlib.ticker import LogFormatter, NullFormatter, FuncFormatter
+from pangal.filters import nice_filter_names
 
-
-def inspect_photometry(self,         
+def inspect_photometry(      
+               galaxy,
                band,              # One band only
                region,            # One region only
                units='mJy',        # units of output 
                threshold_quantile=0.9,
                n_background_regions=500,
                background_exclude_regions = [],     
+               region_plot_cmap='plasma',
+               background_plot_cmap='viridis',
+               bad_pixel_color='black',
                ):
     """
     Perform aperture photometry with sky subtraction and error estimation.
@@ -400,6 +406,7 @@ def inspect_photometry(self,
     hdu : FITS HDU, optional
         If provided, photometry results will be stored in the HDU.
     """
+    self=galaxy
     
     if band not in self.images.keys():
         raise ValueError(f"Band '{band}' not found in self.images. Available bands: {list(self.images.keys())}")
@@ -419,21 +426,34 @@ def inspect_photometry(self,
 
     wcs = self.images[band].wcs
 
-    # Replace zeros with NaNs for safety
-    #image[image == 0] = np.nan
+
+    
 
     # ---- Plotting ----
-    fig, ax = plt.subplots(2,1,figsize=(14, 14),constrained_layout=True)
+    fig, ax = plt.subplots(1,2,figsize=(18, 10),constrained_layout=True)
             
-    cmap = cm.get_cmap('viridis').copy() #'nipy_spectral'
-    cmap.set_bad(color='pink')         
-    cmap_norm = LogNorm(vmin=np.nanquantile(image[image > 0], 0.05),vmax=np.nanquantile(image, 0.9999))
+    cmap = cm.get_cmap(region_plot_cmap).copy()
+    cmap.set_bad(color=bad_pixel_color)   
+    vmin, vmax = np.nanquantile(image[image > 0], 0.001), np.nanquantile(image, 0.999)
+    ratio = vmax / vmin
+    
+    # Choose normalization
+    if ratio >= 20:  # large dynamic range
+        cmap_norm = LogNorm(vmin=vmin, vmax=vmax)
+    else:  # small range, linear works better
+        cmap_norm = Normalize(vmin=vmin, vmax=vmax)
+    
     im0 = ax[0].imshow(image, origin='lower', cmap=cmap, norm=cmap_norm)
-    cbar = fig.colorbar(im0, ax=ax[0], orientation='horizontal', 
-                        pad=0.05, shrink=0.5, label='counts/s/pixel' if self.images[band].ZP_err != 0 else 'mJy')
+    
+    # --- Create colorbar ---
+    
+    units_label = 'counts/s' if self.images[band].ZP_err != 0 else 'mJy'
+    cbar = fig.colorbar(im0, ax=ax[0], orientation='horizontal', pad=0.02, shrink=0.98, label=units_label+'/pixel')
     cbar.ax.tick_params(labelsize=10)
 
-    ax_reg = ax[0].inset_axes([1.1, 0.05, 0.45, 0.45])
+
+    # --- Inset
+    ax_reg = ax[0].inset_axes([0.68, 0.02, 0.3, 0.3])
 
     ny, nx = region.mask.shape
     ra_max, dec_min = region.wcs.all_pix2world([[0, 0]], 0)[0]
@@ -441,29 +461,27 @@ def inspect_photometry(self,
     x_min, y_min = wcs.all_world2pix([[ra_max, dec_min]], 0)[0]
     x_max, y_max = wcs.all_world2pix([[ra_min, dec_max]], 0)[0]
                 
-    X_low = np.linspace(x_min, x_max, region.mask.shape[1])
-    Y_low = np.linspace(y_min, y_max, region.mask.shape[0])
+    X_low = np.linspace(x_min, x_max, nx)
+    Y_low = np.linspace(y_min, y_max, ny)
     X_low, Y_low = np.meshgrid(X_low, Y_low)
-    ax[0].contour(X_low,Y_low,region.mask,levels=[0.5],
-        colors=region.color,linestyles=region.linestyle,linewidths=region.linewidth,alpha=region.alpha) 
-    add_ra_dec_ticks(ax[0],image,wcs,4,4,12,12,ra_min,ra_max,dec_min,dec_max)
+    for j in [0,1]:
+        ax[j].contour(X_low,Y_low,region.mask,levels=[0.5],
+            colors=region.color,linestyles=region.linestyle,linewidths=region.linewidth,alpha=region.alpha)    
+    
     
     mask = region.project(image, wcs)
     
     im_reg = ax_reg.imshow(mask,origin='lower')
     ax_reg.contour(X_low,Y_low,region.mask,levels=[0.5],
         colors=region.color,linestyles=region.linestyle,linewidths=region.linewidth,alpha=region.alpha) 
-    #cax = inset_axes(ax_reg, width="40%", height="3%", loc='lower center',
-    #                 bbox_to_anchor=(0.5, -0.2, 0.5, 0.5),bbox_transform=ax_reg.transAxes, borderpad=0)    
-    #cbar = fig.colorbar(im_reg, cax=cax, orientation='horizontal',label='mask values')
-    #cbar.ax.tick_params(labelsize=8)
+    ax_reg.set_xticks([])
+    ax_reg.set_yticks([])
 
-  
     # Get bounding box of the True values in the region mask
     mask_indices = np.argwhere(mask > 0)
     y_min_mask, x_min_mask = mask_indices.min(axis=0)
     y_max_mask, x_max_mask = mask_indices.max(axis=0)
-    pad = 10
+    pad = 35
     x_min = max(x_min_mask - pad, 0)
     x_max = x_max_mask + pad
     y_min = max(y_min_mask - pad, 0)
@@ -472,19 +490,35 @@ def inspect_photometry(self,
     ax[0].set_ylim(y_min, y_max)   
     ax_reg.set_xlim(x_min+7, x_max-7)
     ax_reg.set_ylim(y_min+7, y_max-7)
+    ra_max_crop, dec_min_crop = wcs.all_pix2world([[x_min, y_min]], 0)[0]
+    ra_min_crop, dec_max_crop = wcs.all_pix2world([[x_max, y_max]], 0)[0]
+    add_ra_dec_ticks(ax[0],image,wcs,4,4,12,12,ra_min_crop,ra_max_crop,dec_min_crop,dec_max_crop)
+    
 
+
+    
     # Background plot!
-    cmap = cm.get_cmap('plasma').copy()
-    cmap.set_bad(color='white')   
-    cmap_norm = LogNorm(vmin=np.nanquantile(image[image > 0], 0.05),vmax=np.nanquantile(image, 0.95))
-    im1 = ax[1].imshow(image, origin='lower', cmap=cmap, norm=cmap_norm, alpha=1) #0.6
-    label = 'counts/s' if self.images[band].ZP_err != 0 else 'mJy'
-    cbar = fig.colorbar(im1, ax=ax[1], orientation='horizontal', pad=0.02, shrink=0.5, label=label)
-    cbar.ax.tick_params(labelsize=10)
+    cmap = cm.get_cmap(background_plot_cmap).copy()
+    cmap.set_bad(color=bad_pixel_color)   
+    vmin, vmax = np.nanquantile(image[image > 0], 0.01), np.nanquantile(image, 0.999)
+    ratio = vmax / vmin
+    
+    # Choose normalization
+    if ratio >= 10:  # large dynamic range
+        cmap_norm = LogNorm(vmin=vmin, vmax=vmax)
+    else:  # small range, linear works better
+        cmap_norm = Normalize(vmin=vmin, vmax=vmax)
+    im1 = ax[1].imshow(image, origin='lower', cmap=cmap, norm=cmap_norm, alpha=1)
+    cbar = fig.colorbar(im1, ax=ax[1], orientation='horizontal', pad=0.02, shrink=0.98, label=units_label+'/pixel')
+    cbar.ax.tick_params(labelsize=8)
 
-    ax_hist = ax[1].inset_axes([1.1, 0.05, 0.65, 0.65])
-    ax_hist.set_xlabel(label, fontsize=10)
-    ax_hist.tick_params(axis='both', which='major', labelsize=8)
+    # --- Add Ra Dec
+    add_ra_dec_ticks(ax[1],image,wcs,4,4,12,12,)
+
+    
+    ax_hist = ax[1].inset_axes([1.1, 0., 1.05, 0.85])
+    ax_hist.set_xlabel(units_label, fontsize=12)
+    ax_hist.tick_params(axis='both', which='major', labelsize=10)
 
     # Create initial mask for sources to exclude from background estimation
     source_mask = np.zeros_like(image, dtype=bool)
@@ -509,9 +543,11 @@ def inspect_photometry(self,
     else:
         print("No detected sources are masked during background estimation.")
         source_area = 0
-        outer_area = 1  # Avoid divide-by-zero later
+        outer_area = 1      # Avoid divide-by-zero later
 
 
+
+    
 
 
     # region mask
@@ -530,37 +566,39 @@ def inspect_photometry(self,
     # If the field of view is very big compared to the source background can be estimated moving around the regions
     if outer_area > 10 * source_area:
 
-
         back_flux_arr = []
         
-        threshold = np.nanquantile(np.abs(image), threshold_quantile)
+        threshold = np.nanquantile(image[image>-999], threshold_quantile)
 
         mask_indices = np.argwhere(mask > 0)
         mask_values = mask[mask > 0]
 
         good_regions_plotted = 0
         bad_regions_plotted = 0
-        max_good = 100
-        max_bad = 50
+        max_good = 50
+        max_bad = 20
 
-        for _ in range(n_background_regions):
-            dy = random.randint(5, image.shape[0] - 5) #0,1
-            dx = random.randint(5, image.shape[1] - 5)
-            shifted = (mask_indices + [dy, dx]) % image.shape
-
+        while len(back_flux_arr) < n_background_regions:
+            dy = random.randint(- (image.shape[0] - 5), image.shape[0] - 5)
+            dx = random.randint(- (image.shape[1] - 5), image.shape[1] - 5)
+            shifted = mask_indices + [dy, dx]
+            
+            # check if all shifted pixels are inside the image
+            if (shifted[:,0].min() < 0 or shifted[:,0].max() >= image.shape[0] or
+                shifted[:,1].min() < 0 or shifted[:,1].max() >= image.shape[1]):
+                continue  # reject this shift
             shifted_vals = image[shifted[:, 0], shifted[:, 1]]
 
+            # extra checks
             # background region must not overlap with the source region
             if np.any(source_mask[shifted[:, 0], shifted[:, 1]]):
                 continue
 
-            # If more than 10% of the pixels in background region are Nan, skip it
-            if np.isnan(shifted_vals).sum() / nonzero_pixels > 0.1: 
+            # All pixels outside the original image that appear after rotation will be set to -999
+            # Exclude regions that follow even partially outside original image
+            if np.any(np.isclose(shifted_vals, -999, atol=1e-6)):
                 continue
 
-            # If more than 50% of the pixels in background region are zeros, skip it
-            if (shifted_vals == 0).sum() / nonzero_pixels > 0.5: 
-                continue
             
             back_flux = np.nansum(shifted_vals * mask_values)
 
@@ -583,7 +621,7 @@ def inspect_photometry(self,
                 continue
 
             for contour in contours:
-                ax[1].plot(contour[:, 1], contour[:, 0], color=color, linewidth=2.5, alpha=1)
+                ax[1].plot(contour[:, 1], contour[:, 0], color=color, linewidth=1.5, alpha=1)
             # -----
 
         back_flux_arr = np.array(back_flux_arr)
@@ -651,16 +689,7 @@ def inspect_photometry(self,
                 label=rf"$\mu$={mu:.2e}, $\sigma$={sigma:.2e}"
             )
         
-        # Title and legend
-        ax_hist.set_title(
-            f"Background Flux Distribution, {len(back_flux_arr)} regions \n"
-            f"Mean = {np.mean(back_flux_arr):.2e}, Std = {np.std(back_flux_arr):.2e}\n"
-            f"10% = {np.nanquantile(back_flux_arr, 0.1):.2e}, "
-            f"50% = {np.nanquantile(back_flux_arr, 0.5):.2e}, "
-            f"90% = {np.nanquantile(back_flux_arr, 0.9):.2e}",
-            fontsize=10
-        )
-        ax_hist.legend(fontsize=10)
+        ax_hist.legend(fontsize=12)
 
     else:
         # SMALL FOV
@@ -687,35 +716,38 @@ def inspect_photometry(self,
         ax_hist.axvline(x=peak_value,linestyle=':',c='black',lw=2)
 
 
-    
     # photometry
 
     flux = np.nansum(image * mask)
-    print(f"Detected flux: {flux} {label}")
-    print(f"Background: {mean_background_flux} {label}")
-    print(f"Background std: {background_standard_dev} {label}")
+    print(f"Detected flux: {flux} {units_label}")
+
+    # --- Compute Poissonian error
+    exptime = self.images[band].exptime
+    if exptime and flux > 0:
+        exptime = float(self.images[band].header['EXPTIME'])
+        N = flux * exptime
+        poisson_err = np.sqrt(N) / exptime
+    else:
+        poisson_err = 0
+
+    print(f"Poissonian Error {poisson_err} {units_label}")
+
+    # --- Compute Sky Background Error
+    print(f"Background: {mean_background_flux} {units_label}")
+    print(f"Background std: {background_standard_dev} {units_label}")
     
     flux_sky_subtracted = flux - mean_background_flux
 
-    print(f"Sky subtracted flux: {flux_sky_subtracted} {label}")
+    print(f"Sky subtracted flux: {flux_sky_subtracted} {units_label}")
 
     if flux_sky_subtracted < 0:
         print(f"Negative flux: setting flux to 0") 
         flux_sky_subtracted = 0
 
-    exptime = self.images[band].exptime
-    if exptime and flux_sky_subtracted > 0:
-        exptime = float(self.images[band].header['EXPTIME'])
-        total_region_sky_subtracted_counts = flux_sky_subtracted * exptime
-        poisson_err = np.sqrt(total_region_sky_subtracted_counts) / exptime
-    else:
-        poisson_err = 0
-
-    print(f"Poissonian Error {poisson_err} {label}")
     
     total_flux_err = np.sqrt(background_standard_dev**2 + poisson_err**2)
 
-    print(f"Total Error {total_flux_err} {label}")
+    print(f"Total Error {total_flux_err} {units_label}")
                 
     if total_flux_err / flux_sky_subtracted < 0.01:
         total_flux_err = 0.01 * flux_sky_subtracted
@@ -729,12 +761,14 @@ def inspect_photometry(self,
     # So far we worked on counts/s, apart for Herschel
     ZP = self.images[band].ZP
     ZP_err = self.images[band].ZP_err
+    print(f"ZP {ZP} ± {ZP_err}")
 
     #convert all to mag
     mag = ZP - 2.5*np.log10(flux_sky_subtracted)
     mag_err = np.sqrt(ZP_err**2 + (2.5/(np.log(10)*flux_sky_subtracted))**2 * total_flux_err**2)   #  Here I add calibration error!
 
-
+    print(f"Flux in AB magnitudes = {mag:.4f}  ± {mag_err:.4f}")
+    
     if 'mag' in units:
         flux_sky_subtracted = mag
         total_flux_err = mag_err
@@ -750,11 +784,39 @@ def inspect_photometry(self,
 
     fmt = '.5f' if 'mJy' in units else '.2f' if 'mag' in units else '.2e'
     print(f"### Final value ### \n Flux = {format(flux_sky_subtracted, fmt)} ± {format(total_flux_err, fmt)} {units}, SNR = {flux_sky_subtracted / total_flux_err:.2f}")
-
     print('\n')
+    
+    ax[0].text(
+        0.02, 0.98,
+        f"{nice_filter_names[band]}, region {region.id}\n"
+        f"Flux and Poisson: {flux:.2f} ± {poisson_err:.2f}",
+        transform=ax[0].transAxes,
+        fontsize=15,
+        va="top",  # anchor top since you're near y=0.98
+        bbox=dict(
+            facecolor="white",   # box fill color
+            alpha=0.9,
+            edgecolor="cyan",   # border color
+            boxstyle="square,pad=0.3"  # squared corners with small padding
+        )
+    )
+
+    ax[1].text(
+        0.02, 0.98,
+        f"Background: mean = {np.mean(back_flux_arr):.2f}, std = {np.std(back_flux_arr):.2f}",
+        transform=ax[1].transAxes,
+        fontsize=15,
+        va="top",  # anchor top since you're near y=0.98
+        bbox=dict(
+            facecolor="white",   # box fill color
+            alpha=0.9,
+            edgecolor="cyan",   # border color
+            boxstyle="square,pad=0.3"  # squared corners with small padding
+        )
+    )
+
+
     plt.show()
-
-
 
 
             
